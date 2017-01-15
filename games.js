071b4d3df69ecb8bfaf3bde8993d55209ed3fe64
Mitch Gavan <mitchgavan@gmail.com>
2017-01-15 18:15:09 +1100

    refactor: cleaned up routes

--- games.js
+++ games.js
@@ -15,44 +15,42 @@ gameRouter.param('id', (req, res, next, id) => {
   }
 });
 
-gameRouter.get('/', (req, res) => {
-  res.json(games);
-});
+gameRouter.route('/')
+  .get((req, res) => {
+    res.json(games);
+  })
+  .post((req, res) => {
+    const game = req.body;
+    games.push(game);
+    res.json(game);
+  });
 
-gameRouter.get('/:id', (req, res) => {
-  const game = req.game;
-  res.json(game || {});
-});
+gameRouter.route('/:id')
+  .get((req, res) => {
+    const game = req.game;
+    res.json(game || {});
+  })
+  .put((req, res) => {
+    const update = req.body;
+    if (update.id) delete update.id;
 
-gameRouter.post('/', (req, res) => {
-  const game = req.body;
-  games.push(game);
-  res.json(game);
-});
-
-gameRouter.put('/:id', (req, res) => {
-  const update = req.body;
-  if (update.id) delete update.id;
-
-  const game = _.findIndex(games, { id: parseInt(req.params.id) });
-  if (!games[game]) {
-    res.send();
-  } else {
-    const updatedGame = _.assign(games[game], update);
-    res.json(updatedGame);
-  }
-});
-
-gameRouter.delete('/:id', (req, res) => {
-  const game = _.findIndex(games, { id: parseInt(req.params.id) });
-  if (!games[game]) {
-    res.send();
-  } else {
-    const deletedGame = games[game];
-    games.splice(game, 1);
-    res.json(deletedGame);
-  }
-});
-
+    const game = _.findIndex(games, { id: parseInt(req.params.id) });
+    if (!games[game]) {
+      res.send();
+    } else {
+      const updatedGame = _.assign(games[game], update);
+      res.json(updatedGame);
+    }
+  })
+  .delete((req, res) => {
+    const game = _.findIndex(games, { id: parseInt(req.params.id) });
+    if (!games[game]) {
+      res.send();
+    } else {
+      const deletedGame = games[game];
+      games.splice(game, 1);
+      res.json(deletedGame);
+    }
+  });
 
 module.exports = gameRouter;
