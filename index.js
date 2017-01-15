857988dcbd86bec60c1b510fa53aa47e3487eff6
Mitch Gavan <mitchgavan@gmail.com>
2017-01-15 11:33:13 +1100

    feat: added get and post requests
    Local versions only

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
-var getGreeting = require("./getGreeting");
-var getDate = require('./getDate');
-var database = require('./database');
+const getGreeting = require("./getGreeting");
+const getDate = require('./getDate');
+const database = require('./database');
+
+// local db example
+const games = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  var greeting = getGreeting();
-  var date = getDate();
+  const greeting = getGreeting();
+  const date = getDate();
   database.logRequest(greeting, date, (err, count) => {
     res.send(`<h1>${greeting}</h1>
       <p>There have been ${count} visits to this page.<p>`);
   });
 });
 
-app.listen(3000, function () {
+app.get('/games', (req, res) => {
+  res.json(games);
+});
+
+app.get('/games/:id', (req, res) => {
+  const lion = _.find(games, { id: req.params.id });
+  res.json(lion || {});
+});
+
+app.post('/games', (req, res) => {
+  const game = req.body;
+  games.push(game);
+  res.json(game);
+});
+
+app.listen(3000, () => {
   console.log('Console Brawl listening on port 3000!');
 })
 
