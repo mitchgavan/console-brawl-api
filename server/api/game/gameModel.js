f2b32cacf78d8629f42f54bc9b8a1e354ca5e2ae
Mitch Gavan <mitchgavan@gmail.com>
2017-01-21 16:14:45 +1100

    feat: added timestamps to schema

--- server/api/game/gameModel.js
+++ server/api/game/gameModel.js
@@ -1,29 +1,34 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const GameSchema = new Schema({
-  deck: String,
-  id: {
-    type: Number,
-    required: true,
-    unique: true,
+const GameSchema = new Schema(
+  {
+    deck: String,
+    id: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    image: {
+      icon_url: String,
+      medium_url: String,
+      screen_url: String,
+      small_url: String,
+      super_url: String,
+      thumb_url: String,
+      tiny_url: String,
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    platforms: [],
+    site_detail_url: String,
+    images: []
   },
-  image: {
-    icon_url: String,
-    medium_url: String,
-    screen_url: String,
-    small_url: String,
-    super_url: String,
-    thumb_url: String,
-    tiny_url: String,
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  platforms: [],
-  site_detail_url: String,
-  images: []
-});
+  {
+    timestamps: true
+  }
+);
 
 module.exports = mongoose.model('game', GameSchema);
