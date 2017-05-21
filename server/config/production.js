8086138ed3b4e26922847c9345cc6c25b1b73d8a
Mitch Gavan <mitchgavan@gmail.com>
2017-05-21 20:03:32 +1000

    changed mongodb url to environment variable

--- server/config/production.js
+++ server/config/production.js
@@ -1,9 +1,6 @@
 module.exports = {
   db: {
-    // retro platforms database
-    // url: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PW}@ds019063.mlab.com:19063/console-brawl`,
-    // recent platforms database
-    url: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PW}@ds127389.mlab.com:27389/console-brawl-recent`
+    url: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_DB_URL}`
   },
   logging: false
 };
